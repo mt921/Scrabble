33b8bc6ffe6215b3ae4e3e5ba1fbd6f324d6deaa
mt921 <marci.talbot21@imperial.ac.uk>
2023-04-05 22:19:47 +0100

    Need to test if script is working

--- main.js
+++ main.js
@@ -1,5 +1,42 @@
-const grid = document.getElementById("grid");
-const row = document.createElement("div");
-row.className = "row";
+// const grid_columns = 8;
+// const grid_rows = 8;
 
-grid.append(row);
+// //placeholder values
+// let current_ply = 1;
+// let tile_selected = false;
+
+// let current_board = [
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,"A",0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0],
+// ];
+
+// document.documentElement.style.setProperty("--grid-rows", grid_rows);
+// document.documentElement.style.setProperty("--grid-columns", grid_columns);
+
+// const grid = document.getElementById("grid");
+
+// //making the grid
+// R.range(0, grid_rows).forEach(function (row_index) {
+//     const row = document.createElement("div");
+//     row.className = "row";
+//     R.range(0, grid_columns).forEach(function (column_index) {
+//         const cell = document.createElement("div");
+//         cell.className = "cell";
+//         cell.textContent = `${current_board[row_index][column_index]}`;
+//         cell.onclick = function () {
+//             if (tile_selected === true) {
+//                 cell.textContent = `${current_ply}`;
+//                 cell.classList.add("tile");
+//                 tile_selected = false;
+//             }
+//         };
+//         row.append(cell);
+//     });
+//     grid.append(row);
+// });
