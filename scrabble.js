f834f9d5bc10376a5e57e53cf6402df9ec09c5f6
mt921 <marci.talbot21@imperial.ac.uk>
2023-04-09 17:54:47 +0100

    game module updates

--- scrabble.js
+++ scrabble.js
@@ -1,3 +1,18 @@
 const Scrabble = Object.create(null);
 
+Scrabble.createGame = function () {
+    return {
+        "currentBoard" : [],
+        "player1Score" : 9,
+        "player2Score" : 0,
+        "hand1" : [],
+        "hand2" : [],
+        "current_bag" : Scrabble.create_new_bag(),
+        "current_orientation" : ["n",0],
+        "current_move" : [],
+        "move_started" : false,
+        "history": []
+    }
+};
 
+export default Object.freeze(Scrabble);
