fb0c33fa6b6a2206ce504b9c904fa7ae8ccf3bad
mt921 <marci.talbot21@imperial.ac.uk>
2023-04-09 12:08:29 +0100

    created hand

--- main.js
+++ main.js
@@ -11,21 +11,36 @@ board = [
   ];
 
 function makeGrid() {
-    let tbl = document.getElementById("grid");
+    let gridTbl = document.getElementById("grid");
     for(let i=0;i<9;i++){
-        let myRow = document.createElement("tr");
-        myRow.id = "row" + i;
-        myRow.className = "row"
-        tbl.appendChild(myRow);
+        let gridRow = document.createElement("tr");
+        gridRow.id = "row" + i;
+        gridRow.className = "row"
+        gridTbl.appendChild(gridRow);
         let rowW = document.getElementById("row"+i);
         for(let j=0; j<9;j++){
-            let myCell = document.createElement("td");
-            myCell.id = [i,j]
-            myCell.className ="cell"
-            myCell.textContent = `${board[i][j]}`;
-            rowW.appendChild(myCell);
+            let gridCell = document.createElement("td");
+            gridCell.id = [i,j]
+            gridCell.className ="cell"
+            gridCell.textContent = `${board[i][j]}`;
+            rowW.appendChild(gridCell);
         }
     }
 }
 
+function makeHand() {
+    let handTbl = document.getElementById("hand");
+    let handRow = document.createElement("tr");
+    handRow.className = "row";
+    handTbl.appendChild(handRow);
+    for(let m=0;m<7;m++){
+        let handCell = document.createElement("td");
+        handCell.id = [0,m];
+        handCell.className="cell";
+        handCell.textContent=`${[0,m]}`;
+        handRow.appendChild(handCell);
+    }
+}
+
+makeHand();
 makeGrid();
