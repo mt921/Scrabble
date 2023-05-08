4658fb0697d4fe75726abd939b5b0259d94c8456
mt921 <marci.talbot21@imperial.ac.uk>
2023-05-08 10:23:47 +0100

    Make grid interactive

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const board = [
     [0,0,"S","T","A","R","T",0,0],
     [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
-    [Scrabble.apple,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0],
   ];
 
@@ -16,6 +16,23 @@ const hand1 = ["A","B","C","D","E","F","G"];
 const hand2 = ["A","B","C","D","E","F","G"];
 const player1Score = 10;
 const player2Score = 5;
+
+function updateGrid() {
+    for(let i=0;i<9;i++){
+        for(let j=0; j<9;j++){
+            let gridCell = document.getElementById([i,j]);
+            gridCell.textContent = `${board[i][j]}`;
+            if (board[i][j] === 0) {
+                gridCell.classList.add("empty");
+                gridCell.classList.remove("tile");
+            }
+            if (board[i][j] !== 0) {
+                gridCell.classList.add("tile");
+                gridCell.classList.remove("empty");
+            }
+        }
+    }
+}
 
 function makeGrid() {
     let gridTbl = document.getElementById("grid");
@@ -29,6 +46,10 @@ function makeGrid() {
             let gridCell = document.createElement("td");
             gridCell.id = [i,j]
             gridCell.className ="cell"
+            gridCell.onclick = function() {
+                board[i][j]="A";
+                updateGrid();
+            }
             gridCell.textContent = `${board[i][j]}`;
             if (board[i][j] === 0) {
                 gridCell.classList.add("empty");
--- scrabble.js
+++ scrabble.js
@@ -2,11 +2,21 @@ const Scrabble = Object.create(null);
 
 Scrabble.createGame = function () {
     return {
-        "currentBoard" : [],
+        "currentBoard" : [
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,"S","T","A","R","T",0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0,0,0],
+        ],
         "player1Score" : 9,
         "player2Score" : 0,
-        "hand1" : [],
-        "hand2" : [],
+        "hand1" : ["A","B","C","D","E","F","G"],
+        "hand2" : ["A","B","C","D","E","F","G"],
         "current_bag" : Scrabble.create_new_bag(),
         "current_orientation" : ["n",0],
         "current_move" : [],
