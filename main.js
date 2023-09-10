948648577d26aed5e76862c0a7f380f1c7af3c14
mt921 <marci.talbot21@imperial.ac.uk>
2023-09-10 21:18:22 +0100

    Add locked & unlocked tiles

--- main.js
+++ main.js
@@ -5,6 +5,9 @@ let gameState = Scrabble.createGameState();
 //placeholder values
 let tileSelected = false;
 
+//NOTE: YOU COULD CONVERT STATE SO THAT IT LINKS TO THE CSS CLASSES
+//THEN YOU CAN ADD THE CLASSES BASED ON THE STATE?
+
 function makeGrid() {
   let gridTbl = document.getElementById("grid");
   for (let i = 0; i < 9; i++) {
@@ -19,20 +22,25 @@ function makeGrid() {
       gridCell.className = "cell";
       //identify the tile value within given board state
       const gridValue = gameState.board[i][j].value;
+      const lockedState = gameState.board[i][j].lockedState;
       gridCell.textContent = `${gridValue}`;
       if (gridValue === 0) {
         gridCell.classList.add("empty");
         gridCell.classList.remove("tile");
       }
-      if (gridValue !== 0) {
-        gridCell.classList.add("tile");
-        gridCell.classList.remove("empty");
-      }
       rowW.appendChild(gridCell);
       //user interaction (clicking functionality)
       gridCell.onclick = function () {
-        if (tileSelected !== false && gameState.board[i][j].value === 0) {
+        //if the user is holding a tile allow them to place it
+        if (tileSelected !== false && gridValue === 0) {
           gameState.board[i][j].value = tileSelected;
+          tileSelected = false;
+          updateGrid();
+        }
+        //if the user is not holding a tile allow them to pick up unlocked tiles
+        if (gridValue !== 0)  {
+          gameState.board[i][j].value = 0;
+          gridCell.textContent = `${gridValue}`;
           tileSelected = false;
           updateGrid();
         }
@@ -70,13 +78,19 @@ function updateGrid() {
     for (let j = 0; j < 9; j++) {
       let gridCell = document.getElementById([i, j]);
       const gridValue = gameState.board[i][j].value;
+      const lockedState = gameState.board[i][j].lockedState;
       gridCell.textContent = `${gridValue}`;
       if (gridValue === 0) {
         gridCell.classList.add("empty");
         gridCell.classList.remove("tile");
-      }
-      if (gridValue !== 0) {
-        gridCell.classList.add("tile");
+        gridCell.classList.remove("tile-unlocked");
+      } else if (gridValue !== 0 && lockedState) {
+        gridCell.classList.add("tile-locked");
+        gridCell.classList.remove("empty");
+        gridCell.classList.remove("tile-unlocked");
+      } else {
+        gridCell.classList.add("tile-unlocked");
+        gridCell.classList.remove("tile-locked");
         gridCell.classList.remove("empty");
       }
     }
